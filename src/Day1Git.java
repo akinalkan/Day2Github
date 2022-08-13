3e0acbf0c4f8ab7a3488ef7e009eda0e851963ef
Akin Alkan <akinalkan45@gmail.com>
2022-08-13 23:18:47 +0300

    V1

---
+++ src/Day1Git.java
@@ -0,0 +1,8 @@
+public class Day1Git {
+    public static void main(String[] args) {
+        System.out.println("Git 1. Deneme");
+        System.out.println("Git 2. Deneme");
+        System.out.println("Git 3. Deneme");
+        System.out.println("Hello Git 4. Deneme");
+    }
+}
