ca229595a319f97deedf247c8c4f4bb514b83b05
Akin Alkan <akinalkan45@gmail.com>
2022-08-15 23:04:44 +0300

    C9

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -24,6 +24,8 @@ public class Day2GitGithub {
         System.out.println("akin branc'i 1. degisikligi");
 
         System.out.println("master branc'i 2. degisikligi");
+        System.out.println("akin branc'i 2.degisikligi");
+
 
 
     }
