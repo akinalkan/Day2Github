d2d9abbdb69c5cff3ae561c2b1ab17aa3f5f24fd
Akin Alkan <akinalkan45@gmail.com>
2022-08-15 21:02:25 +0300

    V2

---
+++ src/Day2GitGithub.java
@@ -0,0 +1,17 @@
+public class Day2GitGithub {
+    /*
+    1-) git init -->Local repo olusturma icin:Yani .git ile klasorumuzun icindeki dosyalari
+    iliskilendirmek icin kullanilir.
+    2-) git add . -->Working space'den(Yani yerel)dosyalarimi
+    Staging area'ya(Yani commit'lemek icin beklenen yer) gonderir
+    3-) git status --> Working space'deki ve staging area'da ki durumu gosterir
+    4-) git commit -m "Mesaj" -->Staging Area;dan commit stora dosyalarimi
+     gondermek icin kullanilir.(commit YAni version[surum] olusturmus olurum)
+    5-) git push --> Uzak repo'ya (Yani remote -Github) gondermek icin kullandigimiz kod.
+    Yalniz git push komutunu direk kullanmak istersek bir kez :
+                    git remote add origin adress
+                    git push -u origin master
+     Not:Yukaridaki 2 komutu tek seferde kullandiktan sonra 2. commit'lerim icin sadece
+     git push  kullaniriz
+     */
+}
