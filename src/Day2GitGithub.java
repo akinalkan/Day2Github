f70c1842f10def41d6e3474561247aa6e1906047
Akin Alkan <akinalkan45@gmail.com>
2022-08-15 23:17:14 +0300

    A2

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -18,6 +18,15 @@ public class Day2GitGithub {
         System.out.println("Github'i da ogrendim");
         System.out.println("2. islem");
         System.out.println("3. islem");
-        // Github ogrendim
+        // Github ogrendim.Degisiklik
+        System.out.println("Branch deneme 1");
+        System.out.println("master branc'i 1. degisikligi");
+        System.out.println("akin branc'i 1. degisikligi");
+
+        System.out.println("master branc'i 2. degisikligi");
+        System.out.println("akin branc'i 2.degisikligi");
+
+
+
     }
 }
